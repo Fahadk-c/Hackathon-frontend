299c542529977b8b9da40e51b3d46271c75b1614
Fahadk-c <fahad@iocod.com>
2025-05-31 15:01:51 +0530

    updated lockfile

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- Your component template content -->
+   Home
+</template>
+
+<script setup>
+// Inline middleware using definePageMeta
+definePageMeta({
+  middleware: [
+    function ({ redirect }) {
+      return redirect('/app')
+    }
+  ]
+})
+</script>
