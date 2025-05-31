3bbc583c9020fd335fbcbe44518cfebd6a4bdc6c
hari-iocod <harikrishnan@iocod.com>
2025-05-31 12:17:15 +0530

    api for pending

--- pages/app/manual-checking/past-reviews.vue
+++ pages/app/manual-checking/past-reviews.vue
@@ -1,13 +1,29 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+const selectedTab = ref('approved');
+
+async function changeType(type: string) {
+
+  const response = await $fetch(`${config.public.apiBase}/review?status=${type}`, { method: 'GET'})
+
+}
+watch(selectedTab, (newVal) => {
+    changeType(newVal);
+});
+
+
+</script>
+
 <template>
-    <div class="w-full p-5 flex flex-col gap-3">
-    <ManualCheckingPastCard/>
-    <ManualCheckingPastCard/>
-    <ManualCheckingPastCard/>
-    <ManualCheckingPastCard/>
-    <ManualCheckingPastCard/>
-    <ManualCheckingPastCard/>
-    <ManualCheckingPastCard/>
-    <ManualCheckingPastCard/>
-    <ManualCheckingPastCard/>
+    <div class="p-5">
+        <Tabs v-model="selectedTab" class="w-[215px] mb-5">
+            <TabsList class="grid w-full grid-cols-2 h-[44px]">
+                <TabsTrigger value="approved">Approved</TabsTrigger>
+                <TabsTrigger value="rejected">Declined</TabsTrigger>
+            </TabsList>
+        </Tabs>
+        <div class="w-full flex flex-col gap-3">
+            <ManualCheckingPastCard v-for="i in 9" :key="i" />
+        </div>
     </div>
 </template>
